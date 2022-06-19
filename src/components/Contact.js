55a19200cdfd6cb4e76d45b99e3ba73b87e97cb6
arif nur rahman <arnu12rahman@gmail.com>
2022-06-19 10:15:19 +0700

    challenge-09 react

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,21 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import "./Contact.css";
 
-const Contact = () => {
+const Contact = ({ data }) => {
     return (
-        <></>
-    )
-}
+        <div className="card">
+            <img className="card-image" src={data.photo} alt={data.name} />
+            <div className="card-content">
+                <h5 className="card-title">{data.name}</h5>
+                <div className="card-body}">
+                    <h2 className="card-phone">{data.phone}</h2>
+                    <p className="card-email">{data.email}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
     return (
-        <></>
-    )
-}
+        <header>
+            <div className="header-content">
+                <h1 className="header-title">Call a Friend</h1>
+                <p className="header-desc">your friendly contact app</p>
+                <hr className="header-hr" />
+            </div>
+        </header>
+    );
+};
 
 export default Header;
